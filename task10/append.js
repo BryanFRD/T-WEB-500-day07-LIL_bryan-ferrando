9d9c64d6e6296a2e225cd803f1e5eb26ef2d66f4
Bryan <bryan.ferrando@epitech.eu>
2023-10-03 13:57:41 +0100

    task10

--- task10/append.js
+++ task10/append.js
@@ -1,13 +1,18 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const div = document.querySelector('div:first-of-type');
+const appendFunction = () => {
+  const button = document.querySelector('button');
 
-button.addEventListener('click', () => {
-  if(input.value.length === 0)
-    return;
-  
+  button.addEventListener('click', () => handleClick(listItem.value));
+}
+
+const handleClick = (text) => {
+  if(text.length === 0)
+      return;
+    
+  const div = document.querySelector('div:first-of-type');
   const d = document.createElement('div');
-  d.innerText = input.value;
+  d.innerText = text;
   div.appendChild(d);
-  input.value = "";
-});
+  listItem.value = "";
+}
+
+appendFunction();
