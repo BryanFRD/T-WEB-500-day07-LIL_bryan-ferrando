82f723a2d0d6c614820001227320eee25215d4ed
Bryan <bryan.ferrando@epitech.eu>
2023-10-03 14:47:43 +0100

    task12

---
+++ task12/script.js
@@ -0,0 +1,16 @@
+const sFuction = () => {
+  const form = document.querySelector('form');
+  const ul = document.querySelector('ul');
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = new FormData(form);
+    const li = document.createElement('li');
+    li.innerText = data.get('text');
+    li.classList.add(data.get('selector'));
+    ul.appendChild(li);
+    form.reset();
+  });
+}
+
+sFuction();
