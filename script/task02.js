a86718266bfac6a8bb6b424120ec96c6cedc9fd7
Bryan <bryan.ferrando@epitech.eu>
2023-10-03 08:57:49 +0100

    task02

--- script/task02.js
+++ script/task02.js
@@ -1,8 +1,8 @@
 button = document.querySelector('footer > div');
 button.addEventListener('click', () => {
   let name = prompt('What\'s your name ?');
-  let confirmedName = prompt(`Are you sure that ${name} is your name ?`);
-  if(name === confirmedName) {
+  let confirmation = confirm(`Are you sure that ${name} is your name ?`);
+  if(confirmation) {
     button.innerText = `Hello ${name} !`;
     alert(`Hello ${name} !`);
   }
