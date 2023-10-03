2b4820df987349341c4946b24ce43d8f593885fc
Bryan <bryan.ferrando@epitech.eu>
2023-10-03 08:47:09 +0100

    task01

--- script/task01.js
+++ script/task01.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('footer > div');
 let count = 0;
+button.innerText = `You clicked 0 times`;
 button.addEventListener('click', () => {
   button.innerText = `You clicked ${++count} times`;
 });
