fc530c9dc0616368939d655d069287914564501f
Bryan <bryan.ferrando@epitech.eu>
2023-10-03 13:34:34 +0100

    task09

---
+++ task09/event.js
@@ -0,0 +1,11 @@
+const firstButton = document.querySelector('button:first-of-type');
+const pList = document.querySelectorAll('p');
+
+firstButton.addEventListener('click', () => {
+  for(let p of pList) {
+    if(p.style.display === 'none')
+      p.style.display = 'block';
+    else
+      p.style.display = 'none';
+  }
+});
